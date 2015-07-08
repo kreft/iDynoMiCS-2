a1e2a09710d5a6c1bdfba0b5503cc2a135b279d9
Robert Clegg <roughhawkbit@gmail.com>
2015-07-08 15:42:56 +0100

    work on ObjectGrid and DoubleGrid

--- src/spatialGrid/DoubleGrid.java
+++ src/spatialGrid/DoubleGrid.java
@@ -1,9 +1,49 @@
 package spatialGrid;
 
-public class DoubleGrid extends ObjectGrid {
-
-	public DoubleGrid() {
-		// TODO Auto-generated constructor stub
+public class DoubleGrid extends ObjectGrid
+{
+	/*************************************************************************
+	 * CONSTRUCTORS
+	 ************************************************************************/
+	
+	public DoubleGrid()
+	{
+		
 	}
-
+	
+	public void init(int[] nVoxel, int[][] padding, Double[] resolution)
+	{
+		super.init(nVoxel, padding, resolution);
+		this.setAll(0.0);
+	}
+	
+	/*************************************************************************
+	 * GRID GETTERS & SETTERS
+	 ************************************************************************/
+	
+	public void setValueAt(int[] position, Double newValue)
+	{
+		this.setValueAt(this.getIndex(position), newValue);
+	}
+	
+	protected Double getValueAt(int index)
+	{
+		return (Double) this.getValueAt(index);
+	}
+	
+	public void setAll(Double newValue)
+	{
+		this.setAll((Object) newValue); 
+	}
+	
+	/*************************************************************************
+	 * GRID METHODS
+	 ************************************************************************/
+	
+	public void multiplyAllBy(Double multiplier)
+	{
+		for ( int index = 0; index < this._voxelsTotal; index++ )
+			this.setValueAt(index, multiplier * this.getValueAt(index));
+	}
+	
 }
--- src/spatialGrid/SoluteGrid.java
+++ src/spatialGrid/SoluteGrid.java
@@ -1,9 +1,22 @@
 package spatialGrid;
 
-public class SoluteGrid extends DoubleGrid {
-
-	public SoluteGrid() {
-		// TODO Auto-generated constructor stub
+public class SoluteGrid
+{
+	protected DoubleGrid _concentration;
+	
+	protected DoubleGrid _diffusivity;
+	
+	/*************************************************************************
+	 * CONSTRUCTORS
+	 ************************************************************************/
+	
+	public SoluteGrid()
+	{
+		
 	}
-
+	
+	public void init()
+	{
+		
+	}
 }
