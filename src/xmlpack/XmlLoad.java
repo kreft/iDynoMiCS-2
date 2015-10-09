24ede7a9ab182d2bbd82f368dc241af24b01e2e3
Bastiaan <baco@env.dtu.dk>
2015-10-09 15:33:57 +0200

    First commit of XML module

---
+++ src/xmlpack/XmlLoad.java
@@ -0,0 +1,149 @@
+package xmlpack;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class XmlLoad {
+	
+	/////////////////////////////
+	// Interfaces
+	/////////////////////////////
+	
+	public interface nodeOperation {
+		void action(Node node);
+	}
+	
+	////////////////////////////
+	// Local classes
+	////////////////////////////
+	
+	////////////////////////////
+	// Methods
+	////////////////////////////
+	
+	public static Element loadDocument(String document)
+	{
+		try {
+			File fXmlFile = new File(document);
+			DocumentBuilderFactory dbF = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = dbF.newDocumentBuilder();
+			Document doc;
+			doc = dBuilder.parse(fXmlFile);
+			doc.getDocumentElement().normalize();
+			return doc.getDocumentElement();
+		} catch (SAXException | IOException | ParserConfigurationException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void forAllNodes(NodeList nodeList, nodeOperation operation) 
+	{
+	    for (int i = 0; i < nodeList.getLength(); i++) 
+		     operation.action(nodeList.item(i));
+	}
+		
+	public static void display(Node node)
+	{
+		display(null, node);
+	}
+	
+	public static void display(String prefix,Node node)
+	{
+		if (node.getNodeType() == Node.ELEMENT_NODE) 
+			display(prefix, (Element) node);
+	}
+	
+	public static void display(Element element)
+	{
+		display(null, element);
+	}
+	
+	public static void display(String prefix, Element element)
+	{
+		String ln = " " + element.getTagName() + " " 
+				+ element.getAttribute("name");
+		if (prefix == null) 
+			System.out.println(ln);
+		else
+			System.out.println(prefix + ln);
+	}
+	
+	public static void displayWithAttributes(String prefix, Node node, 
+			String[] attributes)
+	{
+		if (node.getNodeType() == Node.ELEMENT_NODE) 
+			displayWithAttributes(prefix, (Element) node, attributes);
+	}
+	
+	public static void displayWithAttributes(String prefix, Element element, 
+			String[] attributes)
+	{
+		display(prefix, element);
+		NamedNodeMap a = element.getAttributes();
+		for (int i = 0; i < a.getLength(); i++) {
+			String ln = " |" + a.item(i).getNodeName() + " : " 
+					+ a.item(i).getNodeValue();
+			if (prefix == null) 
+				System.out.println(ln);
+			else
+				System.out.println(prefix + ln);
+		}
+	}
+	
+	public static void displayIfAttribute(Element element, String attribute, String value)
+	{
+		if(element.getAttribute(attribute).toString().equals(value))
+		{
+			displayWithAttributes(null, element, null);
+		}
+	}
+	
+	public static void displayAllChildNodes(Element element)
+	{
+		displayAllChildNodes(null, element);	
+	}
+	
+	public static void displayAllChildNodes(String prefix, Element element)
+	{
+		displayAllChildNodes(null, element, false);
+	}
+	
+	public static void displayAllChildNodes(String prefix, Element element, 
+			Boolean attributes)
+	{
+		if (element.hasChildNodes()) 
+		{
+			NodeList childNodes = element.getChildNodes();
+			forAllNodes(childNodes, new nodeOperation() { 
+				public void action(Node node) 
+				{ 
+					if (attributes)
+						displayWithAttributes(prefix, node, null);
+					else
+						display(prefix, node); 
+					if (node.getNodeType() == Node.ELEMENT_NODE) 
+					{
+						if (prefix == null) 
+							displayAllChildNodes(null, (Element) node, 
+									attributes);
+						else 
+							displayAllChildNodes(prefix + "-", 
+									(Element) node, attributes );
+					}
+				}
+			} );
+		}			
+	}
+}
---
+++ src/xmlpack/XmlTest.java
@@ -0,0 +1,70 @@
+package xmlpack;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.NodeList;
+
+
+
+public class XmlTest {
+
+	public static void main(String[] args) {
+
+		// Load the xml document
+		Element doc = XmlLoad.loadDocument("data.xml");
+
+		
+		// Display document element's general info
+		XmlLoad.displayWithAttributes(null, doc, null);
+		
+		System.out.println("--------------------- ");
+		
+		// display all child nodes (in tree structure, from the element doc, 
+		// with attributes)
+		XmlLoad.displayAllChildNodes("-",doc,true);
+		
+		// filter
+		System.out.println("\nA filter for agents with species 'ecoli': ");
+		NodeList agentNodes = doc.getElementsByTagName("agent"); 
+		for (int i = 0; i < agentNodes.getLength(); i++) 
+		{
+			XmlLoad.displayIfAttribute((Element) agentNodes.item(i),
+					"species","ecoli");
+		} 
+		
+		// load data into model
+		System.out.println("\nLoad data into model: ");
+		
+		////// example class
+		class TestAgent {
+			HashMap<String, Object> _states = new HashMap<String, Object>();
+			public void setState(String name, Object state)
+			{
+				_states.put(name, state);
+			}
+		}
+		
+		// iterate trough all agents in xml file and add them including their
+		// attributes
+		LinkedList<TestAgent> Agents = new LinkedList<TestAgent>();
+		for (int i = 0; i < agentNodes.getLength(); i++) 
+		{
+			TestAgent agent = new TestAgent();
+			NamedNodeMap att = ((Element) agentNodes.item(i)).getAttributes();
+			for(int j = 0; j< att.getLength(); j++)
+			{
+				agent.setState(att.item(j).getNodeName(), 
+						att.item(j).getNodeValue());
+			}
+			Agents.add(agent);
+		}
+		
+		System.out.println("Created: " + Agents.size() + " agents");
+
+		//
+		System.out.println("\nThe end");
+	}
+}
