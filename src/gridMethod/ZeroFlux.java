e4ee7815c1398db659a3bf76e624d18fabc34906
Bastiaan <baco@env.dtu.dk>
2015-12-10 16:00:37 +0100

    fixed zeroFlux implementation

--- src/gridMethod/ZeroFlux.java
+++ src/gridMethod/ZeroFlux.java
@@ -4,8 +4,8 @@ public class ZeroFlux extends ConstantNeumann implements GridMethod {
 
 	// TODO not to sure about this one, originally this was just returning
 	// constantNeumann, we may leave this out, synonyms may cause confusion.
-	ZeroFlux(double gradient) {
-		super(gradient);
+	ZeroFlux() {
+		super(0.0);
 	}
 
 }
