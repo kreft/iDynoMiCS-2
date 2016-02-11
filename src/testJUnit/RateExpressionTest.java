93c678258822c103352d277ffab6949c63dbd5d5
Bastiaan <baco@env.dtu.dk>
2016-02-11 18:46:11 +0100

    Converting to Component construction

--- src/testJUnit/RateExpressionTest.java
+++ src/testJUnit/RateExpressionTest.java
@@ -3,15 +3,14 @@ package testJUnit;
 import org.junit.Test;
 
 import reaction.RateExpression;
-import reaction.term.*;
 
 public class RateExpressionTest {
 
 	@Test
 	public void express()
 	{
-		RateExpression exp = new RateExpression("- test * (test + 1)", null);
-		exp.addTerm(new Monod(0.07), "test");
+		RateExpression exp = new RateExpression("- mu * S / (S + 0.1)", null);
+		exp.build();
 		exp.printEval();
 	}
 }
