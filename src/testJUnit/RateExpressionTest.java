f4c77f655fb4970950bab57c67d503c310772f11
Bastiaan <baco@env.dtu.dk>
2016-02-12 00:16:27 +0100

    Cleaning Rate expressions, additional test case with variables

--- src/testJUnit/RateExpressionTest.java
+++ src/testJUnit/RateExpressionTest.java
@@ -11,12 +11,23 @@ public class RateExpressionTest {
 	@Test
 	public void express()
 	{
-//		RateExpression exp = new RateExpression("25.0 * 3.0", null);
-//		RateExpression exp2 = new RateExpression("1.0 / (1.0 + 0.1)", null);
+		RateExpression exp = new RateExpression("25.0 * 3.0", null);
+		RateExpression exp2 = new RateExpression("1.0 / (1.0 + 0.5)", null);
 		RateExpression exp3 = new RateExpression("(-22.0 * (1.0 * (1.0 * 5.5)))", null);
+		RateExpression exp4 = new RateExpression("mu * S / (K + S)", null);
 		
-//		System.out.println(exp.stringEval() + " = " + exp.component.getValue(new HashMap<String,Double>()));
-//		System.out.println(exp2.stringEval() + " = " + exp2.component.getValue(new HashMap<String,Double>()));
-		System.out.println(exp3.stringEval() + " = " + exp3.component.getValue(new HashMap<String,Double>()));
+		HashMap<String,Double> components = new HashMap<String,Double>();
+		components.put("mu", 0.1);
+		components.put("S", 3.0);
+		components.put("K", 1.0);
+		
+		System.out.println(exp.stringEval() + " = " + exp.component.getValue(
+				new HashMap<String,Double>()));
+		System.out.println(exp2.stringEval() + " = " + exp2.component.getValue(
+				new HashMap<String,Double>()));
+		System.out.println(exp3.stringEval() + " = " + exp3.component.getValue(
+				new HashMap<String,Double>()));
+		System.out.println(exp4.stringEval() + " = " + exp4.component.getValue(
+				components));
 	}
 }
