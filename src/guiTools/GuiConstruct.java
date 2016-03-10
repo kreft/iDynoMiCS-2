19d1a5cd605a381dec261192827f12e337e8e2ce
Bastiaan <baco@env.dtu.dk>
2016-03-10 10:11:55 +0100

    added species lib

--- src/guiTools/GuiConstruct.java
+++ src/guiTools/GuiConstruct.java
@@ -2,10 +2,13 @@ package guiTools;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
@@ -13,7 +16,9 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
 
+import agent.SpeciesLib;
 import idynomics.Compartment;
 import idynomics.GuiLaunch;
 import idynomics.Idynomics;
@@ -34,10 +39,12 @@ public class GuiConstruct {
 		
 		/* simulator pane */
 		JPanel simulatorPane = new JPanel();
+
+		/* simulator pane */
+		JPanel speciesPane = new JPanel();
 		
 		/* compartments */
 		JPanel compartmentPane = new JPanel();
-		compartmentPane.add(textPanel("compartments"));
 				
 		/* start pane */
 		JPanel startPane = new JPanel();
@@ -83,12 +90,39 @@ public class GuiConstruct {
 		}
 		));
 		
+		/* species lib pane content */
+		JComboBox species = new JComboBox();
+		speciesPane.add(selectPanel(species));
+		
+		speciesPane.add(actionButton("refresh species list", "refresh", new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent event)
+			{
+				try
+				{
+					int i = 0;
+					species.removeAllItems();
+					for(String name : Idynomics.simulator.speciesLibrary.getAllSpeciesNames())
+						species.insertItemAt(name, i++);
+				}
+				catch(NullPointerException e)
+				{
+
+				}	
+			}
+		}
+		));
+		
 		
 		/* compartment pane content */
+		compartmentPane.add(textPanel("compartments"));
 		
 		JComboBox box = new JComboBox(new String[]{});
 		compartmentPane.add(selectPanel(box));
 		
+		
+		compartmentPane.add(textPanel("Adding compartments"));
 		JTextArea comp = new JTextArea("compartment name");
 		compartmentPane.add(inputPanel("", comp));
 
@@ -147,6 +181,12 @@ public class GuiConstruct {
 				for(String name : Idynomics.simulator.getCompartmentNames())
 					box.insertItemAt(name, i++);
 				box.setSelectedIndex(0);
+				
+				i = 0;
+				for(String name : Idynomics.simulator.speciesLibrary.getAllSpeciesNames())
+					species.insertItemAt(name, i++);
+				species.setSelectedIndex(0);
+				
 				GuiLaunch.setView(ViewType.SIMCONSTRUCT);
 			}
 			catch(NullPointerException e)
@@ -181,8 +221,7 @@ public class GuiConstruct {
 		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
 		tabbedPane.setEnabledAt(1, false);
 
-		JComponent panel2 = textPanel("Species Library");
-		tabbedPane.addTab("Species Library", null, panel2,
+		tabbedPane.addTab("Species Library", null, speciesPane,
 		                  "Does twice as much nothing");
 		tabbedPane.setMnemonicAt(1, KeyEvent.VK_2);
 		tabbedPane.setEnabledAt(2, false);
@@ -199,7 +238,7 @@ public class GuiConstruct {
 	protected static JPanel inputPanel(String description, JTextArea inputArea)
 	{
 		JPanel panel = new JPanel();
-		panel.setBounds(20, 10, 20, 10);
+		panel.setBounds(10, 10, 10, 10);
 		panel.setLayout(new BorderLayout());
 		panel.setSize(600, 30);
 		
@@ -214,7 +253,7 @@ public class GuiConstruct {
 	
 	protected static JComponent textPanel(String text) {
         JPanel panel = new JPanel();
-        panel.setBounds(20, 10, 20, 10);
+        panel.setBounds(10, 10, 10, 10);
 		panel.setLayout(new BorderLayout());
 		panel.setSize(600, 30);
         JLabel filler = new JLabel(text);
@@ -225,7 +264,7 @@ public class GuiConstruct {
 	
 	protected static JComponent selectPanel(JComboBox box) {
         JPanel panel = new JPanel();
-        panel.setBounds(20, 10, 20, 10);
+        panel.setBounds(10, 10, 10, 10);
 		panel.setLayout(new BorderLayout());
 		panel.setSize(600, 30);
 		
@@ -237,7 +276,7 @@ public class GuiConstruct {
 	protected static JComponent actionButton(String description, String text, ActionListener actionListner)
 	{
 		JPanel panel = new JPanel();
-        panel.setBounds(20, 10, 20, 10);
+        panel.setBounds(10, 10, 10, 10);
 		panel.setLayout(new BorderLayout());
 		panel.setSize(600, 30);
 		
