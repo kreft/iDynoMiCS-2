c7e8695ab2be5737683672ea56fb65cf79a367bd
Stefan Lang <stefan.lang@uni-jena.de>
2016-09-28 12:19:28 +0200

    added OpenGL rendering for polar shapes.

--- src/glRender/AgentMediator.java
+++ src/glRender/AgentMediator.java
@@ -20,7 +20,9 @@ import referenceLibrary.AspectRef;
 import shape.CartesianShape;
 import shape.CylindricalShape;
 import shape.Dimension.DimName;
+import shape.PolarShape;
 import shape.Shape;
+import shape.SphericalShape;
 import surface.Ball;
 import surface.Rod;
 import surface.Surface;
@@ -88,8 +90,6 @@ public class AgentMediator implements CommandMediator {
 	
 	private float[] _orthoX = new float[]{1,0,0}, _orthoY = new float[]{0,1,0},
 					_orthoZ = new float[]{0,0,1}, _rotTemp = new float[16];
-	
-	int[][] voxel_coords;
 
 	/**
 	 * used to set up the open gl camera
@@ -111,17 +111,9 @@ public class AgentMediator implements CommandMediator {
 		/* determine kickback for camera positioning */
 		_kickback = (float) Math.max(_domainLength[0],
 				Math.max(_domainLength[1], _domainLength[2]));
-		
-		ArrayList<int[]> temp = new ArrayList<>();
-		for (int[] cur = _shape.resetIterator();
-				_shape.isIteratorValid(); cur = _shape.iteratorNext() )		
-			temp.add(cur);
-		
-		voxel_coords = new int[temp.size()][3];
-		for (int i=0; i<temp.size(); ++i){
-			System.out.println(Vector.toString(temp.get(i)));
-			voxel_coords[i] = temp.get(i);
-		}
+		/* The domain length is equal to the radius for polar shapes */
+		if (_shape instanceof PolarShape)
+			_kickback *= 2f;
 	}
 	
 	/**
@@ -151,10 +143,7 @@ public class AgentMediator implements CommandMediator {
 		 * draw the domain Shape
 		 *
 		 */
-		if (_shape instanceof CartesianShape){
-			draw((CartesianShape) _shape);
-		}
-//		draw(_shape);
+		draw(_shape);
 		
 		/* 
      	 * Adjust positioning for the domain (prevent the scene from being
@@ -221,6 +210,9 @@ public class AgentMediator implements CommandMediator {
 	}
 	
 	private void draw(Ball ball){
+		/* scale number of slices / stacks proportional to the radius */
+		int stacks = (int)Math.ceil(ball._radius) * _stacks;
+		int slices = (int)Math.ceil(ball._radius) * _slices;
 		_gl.glPushMatrix();
 		applyCurrentColor();
 		double[] loc = GLUtil.make3D(ball._point.getPosition());
@@ -228,7 +220,7 @@ public class AgentMediator implements CommandMediator {
 		GLUquadric qobj = _glu.gluNewQuadric();
 		_glu.gluQuadricDrawStyle(qobj, GLU.GLU_FILL);
 		_glu.gluQuadricNormals(qobj, GLU.GLU_SMOOTH);
-		_glu.gluSphere(qobj, ball._radius, _slices, _stacks);
+		_glu.gluSphere(qobj, ball._radius, slices, stacks);
 		_glu.gluDeleteQuadric(qobj);
 		_gl.glPopMatrix();
 	}
@@ -236,6 +228,11 @@ public class AgentMediator implements CommandMediator {
 	private void draw(Rod rod) 
 	{
 		Tier level = Tier.BULK;
+		
+		/* scale number of slices / stacks proportional to the radius */
+		int stacks = (int)Math.ceil(rod._radius) * _stacks;
+		int slices = (int)Math.ceil(rod._radius) * _slices;
+		
 		 /* first sphere */
 		double[] posA = GLUtil.make3D(rod._points[0].getPosition());
 		 /* second sphere*/
@@ -251,7 +248,7 @@ public class AgentMediator implements CommandMediator {
 		if ( Log.shouldWrite(level) )
 		{
 			Log.out(level, "Constructing Rod with radius " + rod._radius + " and " 
-					+ _slices + " slices, " + _stacks + " stacks" );
+					+ slices + " slices, " + stacks + " stacks" );
 		}
 		GLUquadric qobj = _glu.gluNewQuadric();
 
@@ -259,7 +256,7 @@ public class AgentMediator implements CommandMediator {
 		_gl.glTranslated(posA[0], posA[1], posA[2]);
 		_glu.gluQuadricDrawStyle(qobj, GLU.GLU_FILL);
 		_glu.gluQuadricNormals(qobj, GLU.GLU_SMOOTH);
-		_glu.gluSphere(qobj, rod._radius, _slices, _stacks);
+		_glu.gluSphere(qobj, rod._radius, slices, stacks);
 		
 		/* direction from posB to posA */
 		double[] dp = Vector.minus(posB, posA);
@@ -279,7 +276,7 @@ public class AgentMediator implements CommandMediator {
 				rod._radius, 		/* base */
 				rod._radius, 		/* top */
 				height, 			/* height */
-				_slices, _stacks);
+				slices, stacks);
 		/* restore matrix state before rotation (so we are at point A again)*/
 		_gl.glPopMatrix();
 
@@ -287,20 +284,23 @@ public class AgentMediator implements CommandMediator {
 		_gl.glTranslated(dp[0], dp[1], dp[2]);
 		_glu.gluQuadricDrawStyle(qobj, GLU.GLU_FILL);
 		_glu.gluQuadricNormals(qobj, GLU.GLU_SMOOTH);
-		_glu.gluSphere(qobj, rod._radius, _slices, _stacks);
+		_glu.gluSphere(qobj, rod._radius, slices, stacks);
 		
 		/* clean up */
 		_glu.gluDeleteQuadric(qobj);
 		_gl.glPopMatrix();
 	}
 	
-	private void draw(CartesianShape shape){
+	private void draw(Shape shape){
 		/* save the current modelview matrix */
 		_gl.glPushMatrix();
 		
 		double[] length = GLUtil.make3D(shape.getDimensionLengths());
+		/* scale number of slices / stacks proportional to the radius */
+		int stacks = (int)Math.ceil(length[0]) * _stacks;
+		int slices = (int)Math.ceil(length[0]) * _slices;
 
-		/* set different color / blending for 3 dimensional Cartesian shapes */
+		/* set different color / blending for 3 dimensional Shapes */
 		if (length[2] > 0)
 		{
 			_rgba = new float[] {0.1f, 0.1f, 1.0f};
@@ -318,14 +318,34 @@ public class AgentMediator implements CommandMediator {
 		_gl.glDisable(GL2.GL_DEPTH_TEST); 
 		applyCurrentColor();
 		
-		/* scale y and z relative to x (which we will choose as cube-size) */
-		_gl.glScaled(1, length[1] / length[0], length[2] / length[0]);
+		/* apply different functions for different types */
+		if (shape instanceof CartesianShape){
+			
+			/* scale y and z relative to x (which we will choose as cube-size)*/
+			_gl.glScaled(1, length[1] / length[0], length[2] / length[0]);
 
-		
-		/* draw the scaled cube (rectangle).
-		 * Note that a cube with length 0 in one dimension is a plane 
-		 */
-		_glut.glutSolidCube((float)length[0]);
+			/* draw the scaled cube (rectangle).
+			 * Note that a cube with length 0 in one dimension is a plane 
+			 */
+			_glut.glutSolidCube((float)length[0]);
+			
+		}else if (shape instanceof CylindricalShape){
+
+			/* draw the cylinder.
+			 * Note that a cylinder with height 0 is a circle and only full 
+			 * circles can be drawn at the moment. 
+			 */
+			_glut.glutSolidCylinder(length[0], length[2], slices, 
+					(int)Math.ceil(length[2])); 
+			
+		}else if (shape instanceof SphericalShape){
+			
+			/* draw the sphere.
+			 * Note that only full spheres can be drawn at the moment. 
+			 */
+			_glut.glutSolidSphere(length[0], slices, stacks);
+			
+		}
 		
 		/* make sure Depth test is re-enabled and blend is disabled before
 		 * drawing other objects.
@@ -335,66 +355,6 @@ public class AgentMediator implements CommandMediator {
 
 		_gl.glPopMatrix();
 	}
-	
-//	private void draw(Shape shape){
-//		/* save the current modelview matrix */
-//		_gl.glPushMatrix();
-//		
-//		double[] length = GLUtil.make3D(shape.getDimensionLengths());
-//
-//		/* set different color / blending for 3 dimensional Cartesian shapes */
-//		if (length[2] > 0)
-//		{
-//			_rgba = new float[] {0.1f, 0.1f, 1.0f};
-//			_gl.glEnable(GL2.GL_BLEND);
-//		}
-//		else
-//		{
-//			_rgba = new float[] {0.3f, 0.3f, 0.3f};
-//		}
-//		/**
-//		 * NOTE moved this here since it seems to resolve black lines in domain 
-//		 * square, as long as the domain is drawn first this should not cause
-//		 * any problems.
-//		 */
-//		_gl.glDisable(GL2.GL_DEPTH_TEST); 
-//		applyCurrentColor();
-//		
-//		int[] cur;
-//		double[] centre = new double[3], orig = new double[3],
-//				 pz = new double[3], pz_inside = new double[]{0, 0, 1};
-//		
-//		for (int i = 0; i < voxel_coords.length; ++i)
-//		{			
-//			_gl.glPushMatrix();
-//			
-//			/* update voxel properties */
-//			cur = voxel_coords[i];
-//			shape.voxelCentreTo(centre, cur);
-//			shape.voxelOriginTo(orig, cur);
-//			shape.getVoxelSideLengthsTo(length, cur);
-//			shape.locationTo(pz, cur, pz_inside);
-//			
-//			/* scale second and third dim relative to first dim */
-//			_gl.glScaled(1, length[1] / length[0], length[2] / length[0]);
-//			
-////			glRotated(Vector.minus(pz, orig));
-//			
-//			_gl.glTranslated(centre[0], centre[1], centre[2]);
-//
-//			_glut.glutSolidCube((float)length[0]);
-//			
-//			_gl.glPopMatrix();
-//		}
-//		
-//		/* make sure Depth test is re-enabled and blend is disabled before
-//		 * drawing other objects.
-//		 */
-//		_gl.glEnable(GL2.GL_DEPTH_TEST);
-//		_gl.glDisable(GL2.GL_BLEND);
-//
-//		_gl.glPopMatrix();
-//	}
 	
 	/**
 	 * Sets the current ambient and specular color to <b>this._rgba</b> 
