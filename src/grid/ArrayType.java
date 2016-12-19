44d95fab2a3d52fb67bbcb2c68671d25adce813e
Robert Clegg <roughhawkbit@gmail.com>
2016-12-19 17:48:23 +0000

    Improve description of array types

--- src/grid/ArrayType.java
+++ src/grid/ArrayType.java
@@ -9,22 +9,26 @@ package grid;
 public enum ArrayType
 {
 	/**
-	 * The concentration of, e.g., a solute.
+	 * The concentration of, e.g., a solute. This is always in terms of mass
+	 * per volume (mass<sup>1</sup> length<sup>-3</sup>).
 	 */
 	CONCN,
 	/**
 	 * The diffusion coefficient of a solute. For example, this may be
-	 * lower inside a biofilm than in the surrounding water.
+	 * lower inside a biofilm than in the surrounding water. This is always in
+	 * terms of area per time (length<sup>2</sup> time<sup>-1</sup>).
 	 */
 	DIFFUSIVITY,
 	/**
 	 * A measure of how well-mixed a solute is. A diffusion-reaction should
-	 * ignore where this is above a certain threshold.
+	 * ignore where this is above a certain threshold. This is an arbitrary
+	 * scale with no relation to physical units.
 	 */
 	WELLMIXED,
 	/**
 	 * The rate of production of this solute. Consumption is described by
-	 * negative production.
+	 * negative production. This is always in terms of mass per time 
+	 * (mass<sup>1</sup> time<sup>-1</sup>).
 	 */
 	PRODUCTIONRATE,
 	/**
