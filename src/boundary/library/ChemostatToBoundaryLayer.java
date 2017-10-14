b9562e68b8986cc58dcfcab4a08bd5797e9aadbc
Robert Clegg <roughhawkbit@gmail.com>
2017-10-14 12:19:58 +0100

    Encapsulate agent outflow for chemostat boundaries in single behaviour

--- src/boundary/library/ChemostatToBoundaryLayer.java
+++ src/boundary/library/ChemostatToBoundaryLayer.java
@@ -3,10 +3,14 @@
  */
 package boundary.library;
 
+import java.util.Collection;
+
 import org.w3c.dom.Element;
 
+import agent.Agent;
 import boundary.Boundary;
 import boundary.spatialLibrary.BiofilmBoundaryLayer;
+import boundary.standardBehaviours.DilutionAgentOutflowBehaviour;
 import settable.Settable;
 
 /**
@@ -17,9 +21,18 @@ import settable.Settable;
  */
 public class ChemostatToBoundaryLayer extends Boundary
 {
+	/**
+	 * \brief This boundary's behaviour for grabbing agents to be removed by
+	 * outflow.
+	 * 
+	 * Encapsulated here as it is used by many other chemostat boundaries.
+	 */
+	private DilutionAgentOutflowBehaviour _agentOutflowBehaviour;
+	
 	public ChemostatToBoundaryLayer()
 	{
 		super();
+		this._agentOutflowBehaviour = new DilutionAgentOutflowBehaviour();
 	}
 
 	@Override
@@ -48,4 +61,12 @@ public class ChemostatToBoundaryLayer extends Boundary
 
 	// TODO [Rob 13June2016]: We need to grab agents from the chemostat here,
 	// in a similar way to ChemostatToChemostat, but there is no "flow rate".
+
+
+	@Override
+	public Collection<Agent> agentsToGrab()
+	{
+		return this._agentOutflowBehaviour.agentsToGrab(
+				this._agents, this.getDilutionRate());
+	}
 }
--- src/boundary/library/ChemostatToChemostat.java
+++ src/boundary/library/ChemostatToChemostat.java
@@ -1,16 +1,13 @@
 package boundary.library;
 
 import java.util.Collection;
-import java.util.LinkedList;
-import java.util.List;
 
 import org.w3c.dom.Element;
 
 import agent.Agent;
 import boundary.Boundary;
+import boundary.standardBehaviours.DilutionAgentOutflowBehaviour;
 import idynomics.Compartment;
-import idynomics.Idynomics;
-import linearAlgebra.Vector;
 import settable.Settable;
 
 /**
@@ -21,14 +18,17 @@ import settable.Settable;
 public class ChemostatToChemostat extends Boundary
 {
 	/**
-	 * Tally for the number of agents to be diluted via this boundary (kept at
-	 * zero for inflows).
+	 * \brief This boundary's behaviour for grabbing agents to be removed by
+	 * outflow.
+	 * 
+	 * Encapsulated here as it is used by many other chemostat boundaries.
 	 */
-	protected double _agentsToDiluteTally = 0.0;
+	private DilutionAgentOutflowBehaviour _agentOutflowBehaviour;
 
 	public ChemostatToChemostat()
 	{
 		super();
+		this._agentOutflowBehaviour = new DilutionAgentOutflowBehaviour();
 	}
 
 	@Override
@@ -85,41 +85,13 @@ public class ChemostatToChemostat extends Boundary
 		 * knock the dilution tally down by one.
 		 */
 		super.addOutboundAgent(anAgent);
-		this._agentsToDiluteTally--;
+		this._agentOutflowBehaviour.reduceTallyByOne();
 	}
 
 	@Override
 	public Collection<Agent> agentsToGrab()
 	{
-		List<Agent> out = new LinkedList<Agent>();
-		int nAllAgents = this._agents.getNumAllAgents();
-		if ( (nAllAgents > 0) && (this._volumeFlowRate < 0.0) )
-		{
-			/* 
-			 * This is an outflow: remember to subtract, since dilution out
-			 * is negative.
-			 */
-			this._agentsToDiluteTally -= this.getDilutionRate() * 
-					Idynomics.simulator.timer.getTimeStepSize();
-			int n = (int) this._agentsToDiluteTally;
-			/* Cannot dilute more agents than there are in the compartment. */
-			n = Math.min(n, nAllAgents);
-			int[] nums = Vector.randomInts(n, 0, nAllAgents);
-			for ( int i : nums )
-				out.add(this._agents.chooseAgent(i));
-		}
-		else
-		{
-			/*
-			 * If the agent container is empty, set the tally to zero: we
-			 * shouldn't accumulate a high tally while the compartment is
-			 * waiting for agents to arrive.
-			 * 
-			 * If the flow rate is positive, this is an inflow and so no agents
-			 * to remove.
-			 */
-			this._agentsToDiluteTally = 0.0;
-		}
-		return out;
+		return this._agentOutflowBehaviour.agentsToGrab(
+				this._agents, this.getDilutionRate());
 	}
 }
