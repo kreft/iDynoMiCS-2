502b066ffb4efd24a4e7b247d8574cce451ae637
baseendje <bascee@gmail.com>
2019-08-20 01:08:47 +0200

    removed unnecessary clone

--- src/aspect/Aspect.java
+++ src/aspect/Aspect.java
@@ -109,13 +109,7 @@ public class Aspect implements Instantiable, Settable
     {
     	
     }
-    
-    public Aspect clone()
-    {
-    	return new Aspect ((Object) ObjectFactory.copy(
-				this.aspect ), this.key, this.registry );
-    }
-    
+
     /**
      * Set passed object as aspect for existing aspect object
      * @param aspect
