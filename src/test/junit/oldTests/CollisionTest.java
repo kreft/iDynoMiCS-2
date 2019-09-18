256961a49c48f8ae6abd65f0c9d57f75aada6b36
baseendje <bascee@gmail.com>
2019-09-18 16:24:39 +0200

    updated some collision testing senarios

--- src/test/junit/oldTests/CollisionTest.java
+++ src/test/junit/oldTests/CollisionTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import compartment.Compartment;
 import idynomics.Idynomics;
+import linearAlgebra.Vector;
 import surface.Rod;
 import surface.Voxel;
 
@@ -31,23 +32,38 @@ public class CollisionTest {
 //	}
 	
 	@Test
-	public void periodicTest()
+	public void test3D()
 	{
 		Idynomics.setupSimulator("protocol/unit-tests/empty_5x5.xml");
 		Compartment com = Idynomics.simulator.getCompartment("5x5x5");
 
-		
-		Rod rod = new Rod(
-				new double[] {0.6, 0.6, 0.6}, 	// second point
-				new double[] {1.1, 1.1, 1.1}, 	// first point
-				0.4);						// radius
+		/* tip to tip hit */
+		tiptest(com, 0.6, 1.1, 0.4, 0.0, 0.5);
+		/* flipped tip to tip hit */
+		tiptest(com, 0.0, 0.5, 0.4, 0.6, 1.1);
+		/* tip to tip miss */
+		tiptest(com, 0.9, 1.4, 0.4, 0.0, 0.5);
+	}
+	
+	private boolean tiptest(Compartment com, double lr, double hr, double rr, 
+			double lv, double hv)
+	{
+		return tiptest(com, Vector.vector(3,lr), Vector.vector(3,hr), rr,
+				Vector.vector(3,lv), Vector.vector(3,hv) );
+	}
+	
+	private boolean tiptest(Compartment com, double[] lr, double hr[], double rr, 
+			double lv[], double hv[])
+	{
+
+		Rod rod = new Rod( lr, hr, rr );
 		rod.init(com.getShape().getCollision());
 
-		Voxel vox = new Voxel(
-				new double[] {0.0, 0.0, 0.0},	// lower corner
-				new double[] {0.5, 0.5, 0.5});	// upper corner
+		Voxel vox = new Voxel( lv, hv );
 		vox.init(com.getShape().getCollision());
 
-		System.out.println( rod.collisionWith(vox) );
+		boolean out = rod.collisionWith(vox);
+		System.out.println( out );
+		return out;
 	}
 }
--- src/test/other/TempDebug.java
+++ src/test/other/TempDebug.java
@@ -1,7 +1,5 @@
 package test.other;
 
-import test.junit.*;
-import test.junit.oldTests.BoundaryTest;
 import test.junit.oldTests.CollisionTest;
 
 public class TempDebug {
@@ -9,7 +7,7 @@ public class TempDebug {
 	public static void main(String[] args) {
 		
 		CollisionTest test = new CollisionTest();
-		test.periodicTest();
+		test.test3D();
 	}
 
 }
