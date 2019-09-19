faf5477f1882cce2a1643da3b7e81a65ce41868a
baseendje <bascee@gmail.com>
2019-09-19 21:39:51 +0200

    Added 2D intersection tests

--- src/test/junit/oldTests/CollisionTest.java
+++ src/test/junit/oldTests/CollisionTest.java
@@ -3,33 +3,34 @@ package test.junit.oldTests;
 import org.junit.Test;
 
 import compartment.Compartment;
+import dataIO.Log;
 import idynomics.Idynomics;
 import linearAlgebra.Vector;
 import surface.Rod;
 import surface.Voxel;
 
+/**
+ * \brief: Unit tests for surface intersection.
+ * 
+ * @author Bastiaan
+ *
+ */
 public class CollisionTest {
+	
+	@Test
+	public void test2D()
+	{
+		Idynomics.setupSimulator("protocol/unit-tests/empty_5x5.xml");
+		Compartment com = Idynomics.simulator.getCompartment("5x5x5");
 
-//	@Test
-//	public void periodicTest2D()
-//	{
-//		Idynomics.setupSimulator("protocol/unit-tests/empty_5x5.xml");
-//		Compartment com = Idynomics.simulator.getCompartment("5x5");
-//
-//		
-//		Rod rod = new Rod(
-//				new double[] {0.7, 0.7}, 	// second point
-//				new double[] {1.1, 1.1}, 	// first point
-//				0.5);						// radius
-//		rod.init(com.getShape().getCollision());
-//
-//		Voxel vox = new Voxel(
-//				new double[] {0.0, 0.0},	// lower corner
-//				new double[] {0.5, 0.5});	// upper corner
-//		vox.init(com.getShape().getCollision());
-//
-//		System.out.println( rod.collisionWith(vox) );
-//	}
+		Log.out("2D test");
+		/* tip to tip hit */
+		tiptest(com, 0.6, 1.1, 0.4, 0.0, 0.5, 2);
+		/* flipped tip to tip hit */
+		tiptest(com, 0.0, 0.5, 0.4, 0.6, 1.1, 2);
+		/* tip to tip miss */
+		tiptest(com, 0.9, 1.4, 0.4, 0.0, 0.5, 2);
+	}
 	
 	@Test
 	public void test3D()
@@ -37,19 +38,20 @@ public class CollisionTest {
 		Idynomics.setupSimulator("protocol/unit-tests/empty_5x5.xml");
 		Compartment com = Idynomics.simulator.getCompartment("5x5x5");
 
+		Log.out("3D test");
 		/* tip to tip hit */
-		tiptest(com, 0.6, 1.1, 0.4, 0.0, 0.5);
+		tiptest(com, 0.6, 1.1, 0.4, 0.0, 0.5, 3);
 		/* flipped tip to tip hit */
-		tiptest(com, 0.0, 0.5, 0.4, 0.6, 1.1);
+		tiptest(com, 0.0, 0.5, 0.4, 0.6, 1.1, 3);
 		/* tip to tip miss */
-		tiptest(com, 0.9, 1.4, 0.4, 0.0, 0.5);
+		tiptest(com, 0.9, 1.4, 0.4, 0.0, 0.5, 3);
 	}
 	
 	private boolean tiptest(Compartment com, double lr, double hr, double rr, 
-			double lv, double hv)
+			double lv, double hv, int dim)
 	{
-		return tiptest(com, Vector.vector(3,lr), Vector.vector(3,hr), rr,
-				Vector.vector(3,lv), Vector.vector(3,hv) );
+		return tiptest(com, Vector.vector(dim,lr), Vector.vector(dim,hr), rr,
+				Vector.vector(dim,lv), Vector.vector(dim,hv) );
 	}
 	
 	private boolean tiptest(Compartment com, double[] lr, double hr[], double rr, 
