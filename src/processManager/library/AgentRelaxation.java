0bb9560e948ba6cf1af15ab1018a9eefc6608146
baseendje <bascee@gmail.com>
2020-05-08 19:04:08 +0200

    Work on decompression matrix

--- src/processManager/library/AgentRelaxation.java
+++ src/processManager/library/AgentRelaxation.java
@@ -15,6 +15,7 @@ import dataIO.Log.Tier;
 import expression.Expression;
 import idynomics.Global;
 import idynomics.Idynomics;
+import linearAlgebra.Array;
 import linearAlgebra.Vector;
 import physicalObject.PhysicalObject;
 import processManager.ProcessManager;
@@ -24,6 +25,7 @@ import surface.Point;
 import surface.Rod;
 import surface.Surface;
 import surface.collision.Collision;
+import surface.collision.Decompress;
 import utility.Helper;
 
 /**
@@ -177,6 +179,8 @@ public class AgentRelaxation extends ProcessManager
 	private Expression _spineFunction = 
 			new Expression( "stiffness * ( dh + SIGN(dh) * dh * dh * 100.0 )" );
 	
+	public Decompress decompressionMatrix;
+	
 	/* ************************************************************************
 	 * Initiation
 	 * ***********************************************************************/
@@ -242,6 +246,9 @@ public class AgentRelaxation extends ProcessManager
 		 * is used. */
 		if ( ! Helper.isNullOrEmpty( this.getValue(SPINE_FUNCTION) ) )
 			this._spineFunction = (Expression) this.getValue(SPINE_FUNCTION);
+		
+		decompressionMatrix = 
+				new Decompress(this._agents.getShape().getDimensionLengths(), 1.0);
 	}
 
 	/* ************************************************************************
@@ -265,7 +272,7 @@ public class AgentRelaxation extends ProcessManager
 		double st;
 		/* All located agents in this compartment */
 		Collection<Agent> allAgents = this._agents.getAllLocatedAgents();
-		
+
 		/* if higher order ODE solvers are used we need additional space to 
 		 * write. */
 		switch ( _method )
@@ -282,6 +289,7 @@ public class AgentRelaxation extends ProcessManager
 		{	
 			this._agents.refreshSpatialRegistry();
 			this.updateForces( this._agents );
+			decompressionMatrix.buildDirectionMatrix();
 			
 			/* adjust step size unless static step size is forced */
 			if( !this._dtStatic || this._method == Method.SHOVE )
@@ -455,6 +463,14 @@ public class AgentRelaxation extends ProcessManager
 			/* NOTE: testing purposes only */
 			if (this._gravity)
 				gravityEvaluation(agent, body);
+			
+			for( Point p : ((Body) agent.get(BODY)).getPoints())
+			{
+				decompressionMatrix.addPressure(
+						p.getPosition(), Vector.normEuclid(p.getForce()));
+				p.addToForce(decompressionMatrix.getDirection(p.getPosition()));
+			}
+			
 		}
 	}
 	
---
+++ src/surface/collision/Decompress.java
@@ -0,0 +1,142 @@
+package surface.collision;
+
+import linearAlgebra.Vector;
+
+public class Decompress {
+	
+	public int nDim;
+	public double[] resolution;
+	public double[][][] pressure;
+	public double[][][][] shift; // directionDim, x, y, z
+	public double threshold = 10;
+	public int[] maxima;
+	
+	public Decompress(double[] max, double targetResolution)
+	{
+		nDim = max.length;
+		resolution = new double[3];
+		for( int i = 0; i < nDim; i++ )
+		{
+			resolution[i] = max[i] / Math.ceil(max[i] / targetResolution);
+		}
+		
+		maxima = new int[] { 
+				(int) (max[0] / resolution[0]),
+				(int) (max[1] / resolution[1]),
+				(max.length == 3 ? (int) (max[2] / resolution[2]) : 1)
+				};
+		
+		pressure = new double
+				[maxima[0]]
+				[maxima[1]]
+				[maxima[2]];
+		
+		shift = new double
+				[3]
+				[maxima[0]]
+				[maxima[1]]
+				[maxima[2]];
+	}
+	
+	public void addPressure(double[] location, double amount)
+	{
+		int[] loc = Vector.translate(location, resolution);
+		pressure[loc[0]][loc[1]][loc[2]] += amount/10.0;
+	}
+	
+	public int[] getMop(double[] location)
+	{
+		return Vector.translate(location, resolution);
+	}
+	
+	public double[] getDirection(double[] location)
+	{
+		int[] loc = Vector.translate(location, resolution);
+		return new double[] { 
+				shift[0][loc[0]][loc[1]][loc[2]],
+				shift[1][loc[0]][loc[1]][loc[2]],
+				shift[2][loc[0]][loc[1]][loc[2]] };
+	}
+	
+	public void buildDirectionMatrix()
+	{
+		// clear shift matrix from previous iteration
+		shift = new double
+				[3]
+				[maxima[0]]
+				[maxima[1]]
+				[maxima[2]];
+		
+		// build new shift matrix
+		for( int i = 0; i < pressure.length; i++ )
+			for( int j = 0; j < pressure[0].length; j++ )
+				for( int k = 0; k < pressure[0][0].length; k++ )
+		{
+			int [] location = new int[] { i, j, k };
+			if( pressure[i][j][k] != 0.0)	
+				for( int l = 0; l < resolution.length; l++)
+				{
+					int range[] = shiftRange(location, l);
+					for( int m = range[0]; m < location[l]; m++)
+					{
+						directionAdd( l, m, location, 
+								-pressure[i][j][k]);
+					}
+					for( int m = range[1]; m > location[l]; m--)
+					{
+						directionAdd( l, m, location, 
+								pressure[i][j][k]);
+					}
+				}
+		}
+		// reset pressure matrix for next step
+		pressure = new double
+				[maxima[0]]
+				[maxima[1]]
+				[maxima[2]];
+	}
+	
+	public void directionAdd(int dim, int mod, int[] loc, double val)
+	{
+		switch(dim) {
+		case 0 :
+			shift[dim][mod][loc[1]][loc[2]] += val;
+			break;
+		case 1 :
+			shift[dim][loc[0]][mod][loc[2]] += val;
+			break;
+		case 2 :
+			shift[dim][loc[0]][loc[1]][mod] += val;
+			break;
+	}
+
+	}
+
+	public int[] shiftRange(int[] location, int dimension)
+	{
+		int[] range = new int[] { location[dimension] , location[dimension] };
+		int[] temp = Vector.copy(location);
+		
+		double value = Double.MAX_VALUE;
+		while( value > threshold && range[0] > 0 )
+		{
+			range[0]--;
+			temp[dimension] = range[0];
+			value = pressure[temp[0]][temp[1]][temp[2]];
+		}
+		if( range[0] == 0 && value > threshold)
+			range[0] = location[dimension];
+		
+		value = Double.MAX_VALUE;
+		while( value > threshold && range[1] < maxima[dimension]-1 )
+		{
+			range[1]++;
+			temp[dimension] = range[1];
+			value = pressure[temp[0]][temp[1]][temp[2]];
+		}
+		if( range[1] == maxima[dimension] && value > threshold)
+			range[1] = location[dimension];
+		
+		return range;
+	}
+}
