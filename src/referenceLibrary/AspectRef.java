431ac0ffe8a713df85c56c92516ea49bb170df79
Timothy Foster (PhD Biosciences FT) <TRF896@student.bham.ac.uk>
2024-07-08 18:21:01 +0100

    Some epithelium and transfer reaction-related changes, and the current versions of the Munoz-Tamayo-derived simulation

--- src/referenceLibrary/AspectRef.java
+++ src/referenceLibrary/AspectRef.java
@@ -47,6 +47,8 @@ public class AspectRef
 	 */
 	public final static String deathday = "deathday";
 	
+	public final static String deathMass = "deathMass";
+	
 	/**
 	 * TODO
 	 */
@@ -81,6 +83,11 @@ public class AspectRef
 	 * TODO
 	 */
 	public final static String isEpithelial = "#isEpithelial";
+	
+	/**
+	 * The epithelium to which an epithelial agent belongs
+	 */
+	public final static String epithelium = "epithelium";
 	
 	/**
 	 * TODO
@@ -169,6 +176,12 @@ public class AspectRef
 	 * TODO
 	 */
 	public final static String agentVolumeDistributionMap = "volumeDistribution";
+	
+	
+	/**
+	 * TODO
+	 */
+	public final static String agentSurfaceDistributionMap = "surfaceDistribution";
 
 	/**
 	 * TODO
@@ -702,6 +715,11 @@ public class AspectRef
 	public static final String directionalDivision = "directionalDivision";
 
 	public static final String removed = "removed";
+	
+	public static final String fewNeighboursRemoval = "fewNeighboursRemoval";
+	
+	public static final String fewNeighboursRemovalProbability
+		= "fewNeighboursRemovalProbability";
 
 	public static final String unlinkProbabillity = "unlinkProbabillity";
 
@@ -726,4 +744,15 @@ public class AspectRef
 	public static final String autoVcycleAdjust = "autoVcycleAdjust";
 
 	public static final String absoluteValue = "absoluteValue";
+	
+	public static final String volumeChange = "volumeChange";
+	
+	public static final String volumeTarget = "volumeTarget";
+	
+	public static final String soluteConcentrations = "soluteConcentrations";
+	
+	public static final String soluteMasses = "soluteMasses";
+	
+	public static final String thresholdVolume = "thresholdVolume";
+
 }
