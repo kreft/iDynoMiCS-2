e3fe60a363a99b9a8fd408c0f85a713a0345e4b0
bcockx <bastiaan.cockx@bam.de>
2026-02-23 12:55:07 +0100

    Fixed missing sign for abs ChemostatOut

--- src/idynomics/Global.java
+++ src/idynomics/Global.java
@@ -131,7 +131,7 @@ public class Global extends ParameterSet
 	/**
 	 * Version description.
 	 */
-	public static String version_description = "July 2025";
+	public static String version_description = "Developer build February 2026";
 	
 	/**
 	* Version number of this iteration of iDynoMiCS - required by update
@@ -139,7 +139,7 @@ public class Global extends ParameterSet
 	 *
 	 * suggested Major.Minor.YYMMDD
 	*/
-	public static String version_number = "2.0.250710";
+	public static String version_number = "2.0.260223";
 	
 	/**
 	 * default output location
